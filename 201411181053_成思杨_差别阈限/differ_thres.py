2ac476376dd61938121d240dff5a272341fceb3e
ChengSiyang <chengsiyang@mail.bnu.edu.cn>
2017-01-26 22:43:26 +0800

    201411181053_成思杨_差别阈限

---
+++ 201411181053_成思杨_差别阈限/differ_thres.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 26 21:52:23 2017
+
+@author: csy
+"""
+
+from PIL import Image
+from matplotlib import pylab
+from pylab import *
+import os
+im = Image.open('image00.jpg').convert('L')
+img = array(im)
+img = 0.5*img + 30
+
+for i in range(1,17):
+    img = img + 3
+    image = Image.fromarray(uint8(img)).resize((400,300))
+    image.save('image'+str(i-8)+'.jpg')
+
+PicName = os.listdir()
+print("<item grey>")
+j = 0
+for i in range(0, len(PicName)-1):
+    if (i>8)or(i<7):
+        j = j+1
+        print('  ' + '/' + str(j) + ' = ' + '"' + PicName[i] + '"')
+print("</item>")
+print("\n")
